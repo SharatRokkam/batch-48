c8d99ac478f6f4fa8a2d68bcdabd43d1864ae896
SharatRokkam <rokkamsharath1@gmail.com>
2025-06-16 10:29:55 +0530

    introduction js

---
+++ javascript/introduction/script.js
@@ -0,0 +1,45 @@
+// console.log("Hello World");
+
+// alert("Form Submitted Successfully");
+
+// prompt("Enter your name");
+
+// Variables - Basic / important  (110%)
+
+// var myName = "Sharat"; //var should be avoided
+// let yourName = "Something";
+// const newNumber = 50;
+
+// Rules to define or create a variable
+
+// 1. variable name should not start with number
+// 1myName , my Name ,
+//$myName, my_Name, my-Name
+
+// 2. Reserved keyword should not be used...
+// var var = 10; if, this, else, string, number
+
+// 3. JavaScript is case-sensitive, meaning myVar and myvar are treated as different variables.
+
+// {
+//   var student_name = "Akshay";
+
+//   // var declared = initialization
+//   var student_name = "Sai Teja";
+// }
+
+// console.log(student_name);
+
+// {
+//   let employee_name; //declaration
+//   employee_name = "John"; //initialization
+
+//   employee_name = "Akhil"; //re-initialization
+// }
+// console.log(employee_name);
+
+// const roll_no = 450;
+// roll_no = 400;
+// console.log(roll_no);
+
+const employee_name = "jalfjlaj";
