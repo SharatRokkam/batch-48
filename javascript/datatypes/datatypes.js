8ed4957254979b73f024e137452478539e3dca9d
SharatRokkam <rokkamsharath1@gmail.com>
2025-06-18 10:16:46 +0530

    datatypes

---
+++ javascript/datatypes/datatypes.js
@@ -0,0 +1,74 @@
+// Number
+
+// let price = 999.0;
+// Normal way
+// let myNumber = 100;
+// console.log(typeof myNumber);
+
+// Number constructor - new and first letter would be capital
+// let score = new Number(100);
+// console.log(score);
+
+// String
+
+// let fruit = "banana";
+
+// let veggie = new String("bitter gourd");
+
+// console.log(fruit);
+// console.log(veggie);
+
+// Boolean - true or false
+
+// let isLoggedIn = true;
+
+// let isStudent = new Boolean(false);
+
+// console.log(isLoggedIn);
+// console.log(isStudent);
+
+// What is the difference b/w null and undefined ??(IMP)
+
+// console.log(something);
+// var something = 10;
+
+//intentional absence of the value = null;
+// what is the typeof novalue?
+
+// Almost everything in js is object
+// let novalue = null;
+// console.log(typeof novalue);
+
+// let big_int = 284804828402840820480284028408202084208408;
+// console.log(big_int);
+
+// let a = 10;
+// var a1 = 10;
+// console.log(a == a1);
+
+// Symbol is always unique and it gets a different memory space all the time...
+
+// let key = Symbol("key1");
+// let key1 = Symbol("key1");
+// console.log(key == key1);
+
+//Non Primitive data types
+
+// Array : collection of data types [] --- 0
+
+// let any_type = [1, "value", true, null, undefined, [], {}];
+
+// console.log(any_type[2]);
+
+// Object are collection of key and value pair - {}
+
+// const employee = {
+//     name : "Akshay",
+//     age : 24,
+//     isAadhaar : true,
+//     havePassport : false,
+//     newFunc : function (){}
+// }
+
+// console.log(employee.isAadhaar);
+// console.log(employee.newFunc);
