a3f4b2a805a713c1403dd5d7e1a2abc16e353731
SharatRokkam <rokkamsharath1@gmail.com>
2025-06-25 10:09:09 +0530

    function

---
+++ javascript/functions/script.js
@@ -0,0 +1,33 @@
+// Function Declaration
+// function add(a, b) {
+//   console.log(a + b);
+// }
+
+// add(10, 20);
+
+// Function Expression
+
+// let multiply = function (first, second) {
+//   return first * second;
+// };
+
+// console.log(multiply(5, 8));
+
+// Arrow Function(ES6) - concise way of creating function
+// let sub = (a, b) => console.log(a - b);
+
+// sub(10, 5);
+
+// let maths = (a, b) => {
+//   console.log(a + b);
+//   console.log(a - b);
+//   console.log(a * b);
+//   console.log(a / b);
+// };
+
+// maths(40, 5);
+
+// IIFE - Immediately Invoked Function (Self invoking function)
+// (function (a, b) {
+//   console.log(a + b);
+// })(8, 10);
