ecdb2dd6113ed753fbafb196145c0d7482f368a9
SharatRokkam <rokkamsharath1@gmail.com>
2025-06-27 10:01:57 +0530

    loops

--- javascript/functions/script.js
+++ javascript/functions/script.js
@@ -7,11 +7,11 @@
 
 // Function Expression
 
-// let multiply = function (first, second) {
+// let multiply = function (first, second = 10) {
 //   return first * second;
 // };
 
-// console.log(multiply(5, 8));
+// console.log(multiply(5));
 
 // Arrow Function(ES6) - concise way of creating function
 // let sub = (a, b) => console.log(a - b);
@@ -31,3 +31,36 @@
 // (function (a, b) {
 //   console.log(a + b);
 // })(8, 10);
+
+// var a = 10;
+
+// function num() {
+//   let a = 10;
+//   console.log(a);
+// }
+
+// console.log(a);
+// num();
+
+// find the cube
+
+// let cube = (num) => console.log(num * num * num);
+
+// cube(3);
+
+// area of rectangle
+
+// let area = (l, b) => l * b;
+// console.log(area(10, 5));
+
+let check = (num) => (num % 2 == 0 ? "number is even" : "numbers is odd");
+
+console.log(check(10));
+console.log(check(7));
+console.log(check(0));
+
+// WAF to check if the number is positive or negative
+// WAF to check if the given number is equal or not
+// WAF to check if the given number is divisible by 10 or not
+// WAF to convert minutes into seconds
+// WAF to convert celsius to fahrenheit
---
+++ javascript/loops/script.js
@@ -0,0 +1,97 @@
+// let day = 4;
+
+// switch (day) {
+//   case 1:
+//     console.log("Monday");
+//     break;
+//   case 2:
+//     console.log("Tuesday");
+//     break;
+//   case 3:
+//     console.log("Wednesday");
+//     break;
+//   case 4:
+//     console.log("Thursday");
+//     break;
+//   case 5:
+//     console.log("Friday");
+//     break;
+//   case 6:
+//     console.log("Saturday");
+//     break;
+//   case 7:
+//     console.log("Sunday");
+//     break;
+//   default:
+//     console.log("invalid day");
+// }
+
+// break and continue
+
+// let marks = 67;
+
+// switch (true) {
+//   case marks > 90:
+//     console.log("A+");
+//     break;
+//   case marks > 70 && marks <= 90:
+//     console.log("B+");
+//     break;
+//   case marks > 50 && marks <= 70:
+//     console.log("C+");
+//     break;
+//   case marks > 50 && marks <= 70:
+//     console.log("D+");
+//     break;
+//   case marks < 40:
+//     console.log("Fail");
+//     break;
+//   default:
+//     console.log("invalid marks");
+// }
+
+// Loops
+
+// for
+// for (let index = 0; index <= 10; index++) {
+//   console.log("Hello World");
+// }
+
+// while
+// let i = 0;
+
+// while (i < 10) {
+//   console.log("hello world");
+//   i++;
+// }
+
+// do while
+
+// let i = 0;
+// do {
+//   console.log("Hello world");
+//   i++;
+// } while (i < 6);
+
+// let fruits = ["apple", "banana", "grapes"];
+// let fruits = "pomegranate";
+
+// for (const fruit of fruits) {
+//   console.log(fruit);
+// }
+
+//for in
+
+// let employee = {
+//   name: "Akshay",
+//   id: 2929,
+//   age: 24,
+// };
+
+// for (let key in employee) {
+//   console.log(key, ":", employee[key]);
+// }
+
+// for (let i = 10; i > 0; i--) {
+//   console.log(i);
+// }
