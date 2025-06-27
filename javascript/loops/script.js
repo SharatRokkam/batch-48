ee6c6fec978ce991148594b810b066b0eb8f8aaf
SharatRokkam <rokkamsharath1@gmail.com>
2025-06-27 10:46:46 +0530

    loops

--- javascript/loops/script.js
+++ javascript/loops/script.js
@@ -95,3 +95,55 @@
 // for (let i = 10; i > 0; i--) {
 //   console.log(i);
 // }
+
+// Practice Questions
+
+// 1. Print the table of 5
+// 5 * 1 = 5
+// 5 * 2 = 10
+// 5 * 3 = 15
+
+// let num = 5;
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${num} x ${i} = ${num * i}`);
+// }
+
+// 2.  print the sum of first n numbers
+// let n = 100;
+// let sum = 0;
+
+// for (let i = 1; i <= n; i++) {
+//   sum = sum + i;
+// }
+
+// console.log(sum);
+
+let num = [1, 2, 4, 5, 3, 10, 33, 23];
+let even = 0;
+let odd = 0;
+// count of even = 5
+// count of odd = 3
+
+for (let i = 0; i < num.length; i++) {
+  if (num[i] % 2 == 0) {
+    even++;
+  } else {
+    odd++;
+  }
+}
+console.log("even :", even, "odd :", odd);
+
+// 1. count the number of vowels in a string
+// 2. let marks = {math: 80, physics : 30, bio : 65}
+// output : math : 80%, physics : 30% and bio : 65%
+
+// 3.let expenses = {rent: 10500, grocery : 3200, electricity : 1300}
+//sum all the numeric values in an object : 15000
+
+let people = [
+    {firstName : "John" , secondName : "Don"}
+    {firstName : "Johnny" , secondName : "Johnny"}
+    {firstName : "Janardhan" , secondName : "Mohan"}
+];
+
+// print the firstname of the all the objects
