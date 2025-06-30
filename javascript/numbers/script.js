d51396a6f6a1e6707659ace46f54e1c3ab3501af
SharatRokkam <rokkamsharath1@gmail.com>
2025-06-30 09:44:18 +0530

    numbers

---
+++ javascript/numbers/script.js
@@ -0,0 +1,35 @@
+// Numbers
+
+// first way of creating numbers
+// let a = 10;
+
+//second way of creating numbers
+// let x = new Number(20);
+
+// Methods in Number
+// toFixed
+
+// let price = 40;
+// console.log(price.toFixed(2));
+
+// let num = 355;
+// console.log(num.toString());
+// console.log(num)
+
+// let value = 3.12384;
+// console.log(value.toPrecision(4));
+
+// console.log(Number.isInteger(10));
+// console.log(Number.isInteger("10"));
+
+// console.log(NaN === NaN);
+// console.log(typeof NaN);
+
+// console.log(Number.parseInt("123px"));
+// console.log(Number.parseFloat("12.32kg"));
+
+// console.log(+"123");
+
+// let value = parseInt(prompt("enter any number"));
+
+// Math Method
