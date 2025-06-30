0a186377567713294ce42f91ecb7e5f7a8062dfb
SharatRokkam <rokkamsharath1@gmail.com>
2025-06-30 10:32:17 +0530

    date,math and number

--- javascript/numbers/script.js
+++ javascript/numbers/script.js
@@ -33,3 +33,53 @@
 // let value = parseInt(prompt("enter any number"));
 
 // Math Method
+
+// console.log(Math.abs(-10));
+// console.log(Math.round(1.44));
+// console.log(Math.floor(1.8));
+// console.log(Math.ceil(1.2));
+
+// console.log(Math.min(1, 4, 5, 2));
+// console.log(Math.max(1, 4, 5, 2));
+
+// console.log(Math.pow(2, 3));
+
+// console.log(Math.round(Math.random() + 10)); //0 to 1
+
+// Dice - 1 to 6
+
+// console.log(Math.floor(Math.random() * 6) + 1);
+
+// console.log(-1 / 0);
+// console.log(1 / 0);
+
+// ********************************************************************
+// Date - Object
+
+// const now = new Date();
+// console.log(now);
+// console.log(now.toDateString());
+// console.log(now.toISOString());
+// console.log(now.toLocaleString());
+// console.log(now.toLocaleTimeString());
+
+// const now = new Date("2022-03-20");
+// console.log(now);
+
+// const month = new Date(0);
+// const month = new Date(2023, 3, 30, 20, 0);
+
+// console.log(month.toLocaleString());
+
+const now = new Date();
+// console.log(typeof now);
+
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+console.log(now.getHours());
+console.log(now.getMinutes());
+console.log(now.getSeconds());
+console.log(now.getMilliseconds());
+
+console.log(now.getDay());
